fecf08cacbb300ef3ee24a5f029591da70bfce88
Alex Cruz <cruzalexdavid84@gmail.com>
2026-06-11 17:02:36 -0300

    Clase 9: Parcial 34min

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,8 +3,17 @@
     <h1 class="mb-3">{{ $movie-> title}}</h1>
 
     <div>
-        @if($movie->cover !== null){
-            <img src="{{ \Storage::url($movie->cover) }}" alt="{{ $movie->cover_description }}">
+        {{-- Ek link simbolico "Symlink" es usa porque no se puede acceder a Storage en un hosteo normal --}}
+        {{-- La fachada de "Storage" nos permite interactuar con el servicio del File System
+        Contiene metodos para poder hacer todos los manejos tradicionales en un file system:
+        exists(), url(), (obtiene la url del recurso), etc. --}}
+        {{-- Se tuvo que agregar el ":8000" 
+        IMPORTANTE: a diferencia del metodo url() de laravel, que detecta automaticamente la ruta raiz del dominio 
+        para generar los links, el metodo \Storage::url() necesita que el indiquemos "manualmente" cual es la ruta raiz del dominio.
+        Esta ruta se define en el [.env] en la clave "APP_URL" --}}
+        @if($movie->cover !== null && \Storage::exists($movie->cover)){ {{-- ACa tmb pregunta por si existe el symlink --}}
+            <img src="{{ \Storage::url($movie->cover) }}" alt="{{ $movie->cover_description }}">{{--Usa el symlink para buscar 
+            la imagen  --}}
         }
         @endif
         
